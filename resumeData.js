a192c90c1a3918920552c884f86caef4a1f8693c
Redwanul Haque <redwanulhaque703@gmail.com>
2025-04-18 14:50:59 -0400

    Update resumeData.js

--- resumeData.js
+++ resumeData.js
@@ -1,4 +1,5 @@
 const resumeData = {
+
   name: "Redwanul Haque",
   education: {
     school: "CUNY Queens College",
@@ -6,45 +7,78 @@ const resumeData = {
     duration: "August 2024 – June 2026",
     gpa: "4.0"
   },
+
   experience: [
+
     {
       title: "Graduate Teaching Assistant",
       company: "CUNY Queens College",
       duration: "September 2024 – January 2025",
       responsibilities: [
         "Provided detailed feedback on assignments, improving students' coding skills and code quality.",
-        "Assisted with advanced Python topics such as OOP, data structures, and algorithms.",
-        "Collaborated with the professor to design programming exercises that simulate real-world scenarios."
+        "Assisted with advanced Python topics such as OOP, data structures, and algorithms, enhancing students understanding and performance.",
+        "Collaborated with the professor to design programming exercises that simulate real-world scenarios, enhancing hands-on learning."
       ]
     },
+
     {
       title: "Software Engineer Fellow",
       company: "Headstarter",
       duration: "June 2024 – September 2024",
       responsibilities: [
-        "Conducted code reviews and automated testing (using JUnit).",
-        "Led a project team to deliver a software module ahead of schedule.",
-        "Developed single-page applications using React.js."
+        "Conducted code reviews and automated testing (using JUnit) to ensure software reliability and maintainability, reducing bug-related issues.",
+        "Led a project team to deliver a complex software module within a tight deadline, ensuring all project milestones were achieved ahead of schedule.",
+        "Developed and maintained dynamic single-page applications using React.js, enhancing user experience by implementing responsive and interactive UI components."
+      ]
+    },
+
+    {
+      title: "IT Engineer Internship",
+      company: "Watson Farley & Williams",
+      duration: "January 2024 – May 2024",
+      responsibilities: [
+        "Troubleshot and resolved hardware/software issues, reducing downtime and improving system efficiency.",
+        "Contributed to strategic IT planning and project management, enhancing organizational IT processes.",
+        "Conducted system audits and updates, ensuring compliance with industry standards and best practices."
+      ]
+    },
+
+    {
+      title: "Software Engineer Apprenticeship",
+      company: "Code Nation",
+      duration: "January 2022 – May 2022",
+      responsibilities: [
+        "Showcased a strong commitment to continuous learning and improvement throughout the apprenticeship.",
+        "Demonstrated strong problem-solving skills by tackling complex coding challenges and finding efficient solutions.",
+        "Participated in code reviews to improve code quality and ensure adherence to best practices."
       ]
     }
+
   ],
+
+
   projects: [
+
     {
       name: "Turbo Chat Bot",
       keywords: ["chatbot", "turbo", "ai", "nlp"],
       description: "Developed an AI-powered chatbot using React.js, integrating Google Gemini API for advanced natural language processing and real-time conversational capabilities."
     },
+
     {
       name: "Portfolio Website",
       keywords: ["portfolio", "website", "frontend"],
       description: "Developed a responsive website using HTML, CSS, and JavaScript, featuring interactive interfaces to showcase my software engineering background and enhance user engagement."
     },
+
     {
       name: "Hotel Management System",
       keywords: ["hotel", "management", "reservation"],
       description: "Co-developed hotel management software, implementing 7 software design patterns for seamless integration and designing an intuitive UI for managing reservations within a structured system."
     }
   ],
+
+
   skills: {
     categories: {
       "Programming Languages": ["Java", "Python", "C++", "C", "JavaScript", "SQL"],
